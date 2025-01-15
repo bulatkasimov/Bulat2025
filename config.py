c7ae6054cc85fce9efcfac8fcd0d8a8487d72865
Bulat_Kasimov <qwertybulkaqa@gmail.com>
2025-01-15 11:10:40 +0300

    test1

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('7732480415:AAHo4kLhGMxF356XvZHR1Kub3aLe_3P51Mc')
+bot = telebot.TeleBot("7732480415:AAHo4kLhGMxF356XvZHR1Kub3aLe_3P51Mc")
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=["start"])
 def start(message):
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -11,26 +12,46 @@ def start(message):
     markup.add(btn1)
     bot.send_message(message.from_user.id, "Че как жизнь?", reply_markup=markup)
 
-@bot.message_handler(content_types=['text'])
+
+@bot.message_handler(content_types=["text"])
 def get_text_messages(message):
 
-    if message.text == 'Лера, здарова':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
-        btn1 = types.KeyboardButton('Как стать автором на Хабре?')
-        btn2 = types.KeyboardButton('Просмотреть очередь ожидания')
-        btn3 = types.KeyboardButton('Советы по оформлению публикации')
+    if message.text == "Лера, здарова":
+        markup = types.ReplyKeyboardMarkup(
+            resize_keyboard=True
+        )  # создание новых кнопок
+        btn1 = types.KeyboardButton("Как сделать план на день?")
+        btn2 = types.KeyboardButton("Просмотреть очередь ожидания")
+        btn3 = types.KeyboardButton("Календарь планирования тестирования")
         markup.add(btn1, btn2, btn3)
-        bot.send_message(message.from_user.id, '❓ Задайте интересующий вас вопрос', reply_markup=markup) #ответ бота
+        bot.send_message(
+            message.from_user.id,
+            "❓ Задайте интересующий вас вопрос",
+            reply_markup=markup,
+        )  # ответ бота
+
+    elif message.text == "Как сделать план на день?":
+        bot.send_message(
+            message.from_user.id,
+            "Ну это сложно... Попроси видос, мы про это проводили уже обучение",
+            parse_mode="Markdown",
+        )
+
+    elif message.text == "Посмотреть очередь ожидания":
+        bot.send_message(
+            message.from_user.id,
+            "Просмотреть очередь ожидания можно по "
+            + "[ссылке](https://jira-medmis.bars.group/issues/?filter=14670)",
+            parse_mode="Markdown",
+        )
+
+    elif message.text == "Календарь планирования тестирования":
+        bot.send_message(
+            message.from_user.id,
+            "Календарь планирования тестирования смотри по "
+            + "[ссылке](https://conf.bars.group/pages/viewpage.action?pageId=238701279)",
+            parse_mode="Markdown",
+        )
 
 
-    elif message.text == 'Как стать автором на Хабре?':
-        bot.send_message(message.from_user.id, 'Вы пишете первый пост, его проверяют модераторы, и, если всё хорошо, отправляют в основную ленту Хабра, где он набирает просмотры, комментарии и рейтинг. В дальнейшем премодерация уже не понадобится. Если с постом что-то не так, вас попросят его доработать.\n \nПолный текст можно прочитать по ' + '[ссылке](https://habr.com/ru/sandbox/start/)', parse_mode='Markdown')
-
-    elif message.text == 'Правила сайта':
-        bot.send_message(message.from_user.id, 'Прочитать правила сайта вы можете по ' + '[ссылке](https://habr.com/ru/docs/help/rules/)', parse_mode='Markdown')
-
-    elif message.text == 'Советы по оформлению публикации':
-        bot.send_message(message.from_user.id, 'Подробно про советы по оформлению публикаций прочитать по ' + '[ссылке](https://habr.com/ru/docs/companies/design/)', parse_mode='Markdown')
-
-
-bot.polling(none_stop=True, interval=0) #обязательная для работы бота часть
+bot.polling(none_stop=True, interval=0)  # обязательная для работы бота часть
